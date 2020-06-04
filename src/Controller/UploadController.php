2f0106acd239f57415ce5c146de77a8056f2f259
Frutios <benjamin.tourneur7@gmail.com>
2020-06-04 11:51:21 +0200

    Upload de fichier

---
+++ src/Controller/UploadController.php
@@ -0,0 +1,39 @@
+<?php 
+
+namespace App\Controller;
+
+use App\Form\UploadType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Upload;
+
+
+
+
+class UploadController extends AbstractController
+{
+    /** 
+    * @Route("/upload", name="upload")
+    */
+    public function new(Request $request)
+    {  
+        $upload = new Upload(); 
+        $form = $this->createForm(UploadType::class, $upload);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $file = $upload->getName();
+            $fileName = md5(uniqid()).'.'.$file->guessExtension();
+            $file->move($this->getParameter('csv_directory'), $fileName);
+            $upload->setName($fileName);
+
+        }
+
+        return $this->render('uploader.html.twig', [
+            'form' => $form->createView(),
+            
+        ]);
+
+    }
+}
