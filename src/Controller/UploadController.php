df7fc14f56a0da60a19f0bdbb5807c4c4afe2ba1
Frutios <benjamin.tourneur7@gmail.com>
2020-06-04 11:53:58 +0200

    upload 2

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Controller;
 
@@ -8,32 +8,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Upload;
 
-
-
-
 class UploadController extends AbstractController
 {
-    /** 
+    /**
     * @Route("/upload", name="upload")
     */
     public function new(Request $request)
-    {  
-        $upload = new Upload(); 
+    {
+        $upload = new Upload();
         $form = $this->createForm(UploadType::class, $upload);
         $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $file = $upload->getName();
             $fileName = md5(uniqid()).'.'.$file->guessExtension();
             $file->move($this->getParameter('csv_directory'), $fileName);
             $upload->setName($fileName);
-
         }
 
         return $this->render('uploader.html.twig', [
             'form' => $form->createView(),
             
         ]);
-
     }
 }
--- src/Form/UploadType.php
+++ src/Form/UploadType.php
@@ -9,7 +9,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-
 class UploadType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
