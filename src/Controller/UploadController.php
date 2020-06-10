e949c14f00857413a3d61a5a8c8be1c140b66f61
Frutios <benjamin.tourneur7@gmail.com>
2020-06-10 14:27:50 +0200

    ajout de fonctionnalité de lecture de CSV

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -22,9 +22,20 @@ class UploadController extends AbstractController
             $file = $upload->getName();
             $fileName = uniqid().'.'.$file->guessExtension();
             $file->move($this->getParameter('csv_directory'), $fileName);
+            $openFile = fopen($fileName, 'r');
+            if ($openFile === false) {
+                return $this->render(
+                    'form_upload.html.twig',
+                    ['form' => $form->createView(),
+                    'errorMessage' => 'Le fichier n\' a pas pu être ouvert'
+            
+                    ]
+                );
+            }
+            fgetcsv($openFile, 0, ";", '"');
         }
 
-        return $this->render('uploader.html.twig', [
+        return $this->render('form_upload.html.twig', [
             'form' => $form->createView(),
             
         ]);
