9773301280bf8761193432e5e3e71eaa7c43ab3c
elisematos <elise.matos@hotmail.fr>
2020-06-12 12:24:17 +0200

    Ajout souche/champ selection date/correction retour ligne app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,3 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.scss in this case)
 import '../scss/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
@@ -19,6 +11,6 @@ require('bootstrap');
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
 
-$(document).ready(function() {
+$(document).ready(() => {
     $('[data-toggle="popover"]').popover();
 });
