2c701188aed5f6013b044d45e897629635a310f9
Frutios <benjamin.tourneur7@gmail.com>
2020-06-16 18:00:45 +0200

    correction probleme travis

--- src/Repository/UploadRepository.php
+++ src/Repository/UploadRepository.php
@@ -2,7 +2,7 @@
 
 namespace App\Repository;
 
-use App\Entity\Upload;
+use App\Model\Upload;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
