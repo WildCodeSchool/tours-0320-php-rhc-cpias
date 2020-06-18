34ee2531b15ae13c7c894c5877f5ae6b90a71e22
Charles <charles.chevreuil@gmail.com>
2020-06-18 15:48:54 +0200

    correction travis

--- public/openlayer/finessLayer.js
+++ public/openlayer/finessLayer.js
@@ -1,4 +1,4 @@
-//sources des données servant à remplir le paramètre "source" d'une layer
+// sources des données servant à remplir le paramètre "source" d'une layer
 
 let sourceReg= new ol.source.Vector({
 				url: "/openlayer/kml/regionCentre.kml",
@@ -10,7 +10,7 @@ let sourceReg= new ol.source.Vector({
 let sourceOSM = new ol.source.OSM();
 
 
-//instanciation des différentes Layers (couches) de donnée de la carte. 
+// instanciation des différentes Layers (couches) de donnée de la carte. 
 
 let centre = new ol.layer.Vector({
 	source: sourceReg
@@ -21,7 +21,7 @@ let OSMlayer =  new ol.layer.Tile({
 	});
 
 
-//instanciation de la carte 
+// instanciation de la carte 
 
 let map = new ol.Map({
 	target: 'map',
@@ -32,15 +32,15 @@ let map = new ol.Map({
 	})
 });
 
-//instanciation des Overlay (surcouche), les points sur la carte et le lien sur le nom de l'établissement
+// instanciation des Overlay (surcouche), les points sur la carte et le lien sur le nom de l'établissement
 
-	//instanciation d'une variable qui récupère tt les élément de la class "ponctuel" (1 élément = 1 point sur la carte)
+	// instanciation d'une variable qui récupère tt les élément de la class "ponctuel" (1 élément = 1 point sur la carte)
 
 let ponctuelTableau = document.querySelectorAll(".ponctuel");
 
 Array.prototype.slice.call(ponctuelTableau).forEach(function(element){
 	
-	//récupération des coordonnées dans le html 
+	// récupération des coordonnées dans le html 
 	let coord = (element.querySelector(".coord").innerHTML).split(",");
 	coord[0]=parseFloat(coord[0]);
 	coord[1]=parseFloat(coord[1]);
@@ -48,14 +48,14 @@ Array.prototype.slice.call(ponctuelTableau).forEach(function(element){
 	let coordProj=ol.proj.fromLonLat([coord[0],coord[1]]);
 	
 
-	//instanciation de la surcouche
+	// instanciation de la surcouche
 	var marker = new ol.Overlay({
   		position: coordProj,
   		positioning: 'center-center',
   		element: element.querySelector('#marker'),
   		stopEvent: false
 	});
-	//ajout de la surcouche à la carte
+	// ajout de la surcouche à la carte
 	map.addOverlay(marker);
 
 	// lien sur le nom de l'établissement
