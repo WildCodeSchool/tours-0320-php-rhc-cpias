54caeb09fc6801d592c3feefc313032bfaac87b5
Elise MATOS <35875889+elisematos@users.noreply.github.com>
2020-06-18 10:27:46 +0200

    Update UserFixtures.php

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -17,27 +17,27 @@ class UserFixtures extends Fixture
     }
     public function load(ObjectManager $manager)
     {
-        // Création d’un User de type “utilisateur”
-        $utilisateur = new User();
-        $utilisateur->setUsername('utilisateur');
-        $utilisateur->setRoles(['ROLE_UTILISATEUR']);
-        $utilisateur->setPassword($this->passwordEncoder->encodePassword(
-            $utilisateur,
+        // Création d’un User de type “user”
+        $user = new User();
+        $user->setUsername('user');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
             'userpassword'
         ));
 
-        $manager->persist($utilisateur);
+        $manager->persist($user);
 
-        // Création d’un User de type “responsable”
-        $responsable = new User();
-        $responsable->setUsername('responsable');
-        $responsable->setRoles(['ROLE_RESPONSABLE']);
-        $responsable->setPassword($this->passwordEncoder->encodePassword(
-            $responsable,
-            'responsablepassword'
+        // Création d’un User de type “technician”
+        $technician = new User();
+        $technician->setUsername('technician');
+        $technician->setRoles(['ROLE_TECHNICIAN']);
+        $technician->setPassword($this->passwordEncoder->encodePassword(
+            $technician,
+            'technicianpassword'
         ));
 
-        $manager->persist($responsable);
+        $manager->persist($technician);
 
         $manager->flush();
     }
