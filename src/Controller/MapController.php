2c3130ed6358b784ce0daa2ba611599a9ec7f269
Charles <charles.chevreuil@gmail.com>
2020-06-27 22:06:12 +0200

    pas content

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Form\MapSelectionType;
 use App\Entity\Strain;
 use App\Entity\Finess;
+use App\Entity\Esin;
 use App\Repository\FinessRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -42,13 +43,39 @@ class MapController extends AbstractController
      * @Route("/strainByFiness/{finess}",name="strain_finess", methods={"GET"})
      * @return Response
      */
-    public function strainByFiness(?int $finess): Response
+    public function strainByFiness(int $finess): Response
     {
         $strains = $this->getDoctrine()
             ->getRepository(Strain::class)
             ->findby(['finess'=>$finess]);
         return $this->render('strain/index.html.twig', [
             'strains' => $strains
+        ]);
+    }
+
+    /**
+     * @param int $numFiness
+     * @param int $id
+     * @Route("/strainEsinByFiness/{id}/{numFiness}",name="strainEsin_finess", methods={"GET"})
+     * @return Response
+     */
+    public function strainEsinByFiness(int $id, int $numFiness): Response
+    {
+        $fine = $this->getDoctrine()
+            ->getRepository(Finess::class)
+            ->findAll();
+
+        $strains = $this->getDoctrine()
+            ->getRepository(Strain::class)
+            ->findby(['finess'=>$id]);
+
+        $esins = $this->getDoctrine()
+            ->getRepository(Esin::class)
+            ->findby(['codeFinessEtab'=>$numFiness]);
+
+
+        return $this->render('map/show.html.twig', [
+            'strains' => $strains, 'esins' => $esins, 'finess' => $fine
         ]);
     }
 }
