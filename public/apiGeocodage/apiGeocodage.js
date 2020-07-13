c13645045c6d1027af45c27c49006355cae9d6d5
Charles <charles.chevreuil@gmail.com>
2020-07-13 20:14:32 +0200

    commit intermédiaire

--- public/apiGeocodage/apiGeocodage.js
+++ public/apiGeocodage/apiGeocodage.js
@@ -1,22 +1,39 @@
 const adresseCode = document.querySelectorAll('.select');
 
-const url ="";
-
-function fetchAdresse(){
-    fetch(url)
-        .then(function(response){
-            return response.json();
-        })
-        .then
+function pause(ms)
+{
+    var date = new Date();
+    var curDate = null;
+    do { curDate = new Date(); }
+    while(curDate-date < ms);
 }
 
+let i = 0;
 
-adresseCode.forEach(function(element){
+adresseCode.forEach(function (element) {
     const codePostal = element.querySelector('.code').innerText;
     const adresse = element.querySelector('.adresse').innerText;
     const id = element.querySelector('.id').innerText;
+    const ville = element.querySelector('.ville').innerText;
 
-    const url = `https://api-adresse.data.gouv.fr/search/?q=${adresse}&postcode=${codePostal}`;
 
+    const url = `https://api-adresse.data.gouv.fr/search/?q=${adresse}+${ville}&postcode=${codePostal}&limit=1`;
+
+    fetch(url, { mode: 'cors' })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (profile) {
+            console.log(profile);
+        })
+    if (i%2 == 0){
+        pause(100);
     }
-);
+    if(i%50 == 0){
+        pause(1000);
+    }
+    i++;
+})
+
+
+
